057240af62f01f940a0ac0c313757b48fd7dbd9d
Guilherme Francisquini <guilherme.francisquini@hotmail.com>
2017-12-11 22:22:44 -0200

    primeiro commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Esses pergaminhos estão no antigo e misterioso idioma Booglan. Após muitos anos de estudo, 
+ * os linguistas já conhecem algumas características desse idioma.
+
+ * Primeiramente, as letras Booglan são classificadas em dois grupos: as letras r, t, c, d e b 
+ * são chamadas "letras tipo foo", enquanto que as demais são conhecidas como "letras tipo bar".
+
+ * Os linguistas descobriram que as preposições em Booglan são as palavras de 5 letras que terminam numa letra tipo bar, 
+ * mas onde não ocorre a letra l. Portanto, é fácil ver que existem 49 preposições no Texto A. E no Texto B, quantas 
+ * preposições existem?
+ */
